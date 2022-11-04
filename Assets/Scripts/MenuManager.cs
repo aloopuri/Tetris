4b31e22784b68f46d8dd762f08abe3e8c334acf5
aloopuri <thassan577@gmail.com>
2022-11-04 23:13:28 +0000

    Quit added + renaming script

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     // Value of the game board scene
     public readonly int tetrisGameScene = 1;
 
     public void StartGame() {
         SceneManager.LoadScene(tetrisGameScene);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
     }
 }
